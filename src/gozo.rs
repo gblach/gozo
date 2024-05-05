0c17e97ce9c406e05fa2e7be2f9f5148d63ddf64
Grzegorz Blach <grzegorz@blach.pl>
2024-05-06 00:28:13 +0200

    Initial import

---
+++ src/gozo.rs
@@ -0,0 +1,163 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use async_nats::{ jetstream::kv, Client, HeaderMap, HeaderValue, Message };
+use futures::stream::TryStreamExt;
+use serde::{ Serialize, Deserialize };
+use std::collections::BTreeMap;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::time::{ Duration, SystemTime };
+use tokio::sync::{ Mutex, MutexGuard };
+
+#[derive(Clone)]
+pub struct Sched {
+	pub when: Vec<u64>,
+	pub subject: Vec<String>,
+	pub payload: Vec<bytes::Bytes>,
+	pub id: Vec<Option<String>>,
+	pub id_loc: BTreeMap<String, usize>,
+}
+
+impl Sched {
+	pub fn new() -> Sched {
+		Sched {
+			when: Vec::new(),
+			subject: Vec::new(),
+			payload: Vec::new(),
+			id: Vec::new(),
+			id_loc: BTreeMap::new(),
+		}
+	}
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct KvSched {
+	when: u64,
+	subject: String,
+	payload: bytes::Bytes,
+}
+
+pub type SchedMutex = Arc<Mutex<Sched>>;
+pub type SchedGuard<'a> = MutexGuard<'a, Sched>;
+
+fn now() -> u64 {
+	SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs()
+}
+
+pub async fn replyloop(nats: Client, kv: kv::Store, sched_mutex: SchedMutex) {
+	let mut interval = tokio::time::interval(Duration::new(1, 0));
+
+	loop {
+		interval.tick().await;
+		let mut sched = sched_mutex.lock().await;
+
+		println!("Tick {}: {}", now(), sched.when.len());
+
+		if let Some(when) = sched.when.first() {
+			if *when <= now() {
+				let mut headers = HeaderMap::new();
+				headers.insert("Gozo-Reply", "Yes");
+				if let Some(id) = &sched.id.first().unwrap() {
+					let id = HeaderValue::from_str(id).unwrap();
+					headers.insert("Gozo-Id", id);
+				}
+
+				let _ = nats.publish_with_headers(
+					sched.subject.first().unwrap().clone(), headers,
+					sched.payload.first().unwrap().clone()).await;
+
+				sched.when.remove(0);
+				sched.subject.remove(0);
+				sched.payload.remove(0);
+				if let Some(id) = sched.id.remove(0) {
+					sched.id_loc.remove(&id);
+					let _ = kv.delete(id).await;
+				}
+
+				interval.reset_immediately();
+			}
+		}
+	}
+}
+
+fn get_when(when: &str) -> Result<u64, std::num::ParseIntError> {
+	if when.starts_with('+') {
+		let when = when.trim_start_matches('+');
+		Ok(now() + when.parse::<u64>()?)
+	} else {
+		Ok(when.parse::<u64>()?)
+	}
+}
+
+fn schedule_delete(sched: &mut SchedGuard<'_>, id: String) {
+	if let Some(idx) = sched.id_loc.remove(&id) {
+		sched.when.remove(idx);
+		sched.subject.remove(idx);
+		sched.payload.remove(idx);
+		sched.id.remove(idx);
+	}
+}
+
+pub async fn schedule(kv: kv::Store, sched_mutex: SchedMutex, msg: Message) {
+	if let Some(headers) = msg.headers {
+		if let Some(when) = headers.get("Gozo-When") {
+			if let (Ok(when), Some(reply)) = (get_when(when.as_str()), msg.reply) {
+				let mut sched = sched_mutex.lock().await;
+
+				let id = if let Some(id) = headers.get("Gozo-Id") {
+					schedule_delete(&mut sched, id.to_string());
+					Some(id.to_string())
+				} else {
+					None
+				};
+
+				let idx = sched.when.partition_point(|&x| x <= when);
+
+				sched.when.insert(idx, when);
+				sched.subject.insert(idx, reply.to_string());
+				sched.payload.insert(idx, msg.payload.clone());
+				sched.id.insert(idx, id.clone());
+
+				if let Some(id) = id {
+					sched.id_loc.insert(id.clone(), idx);
+
+					let value = KvSched {
+						when,
+						subject: reply.to_string(),
+						payload: msg.payload,
+					};
+					let value =
+						rmp_serde::to_vec_named(&value).unwrap();
+					let _ = kv.put(id, value.into()).await;
+				}
+			}
+		} else if let Some(id) = headers.get("Gozo-Del-Id") {
+			let mut sched = sched_mutex.lock().await;
+			schedule_delete(&mut sched, id.to_string());
+			let _ = kv.delete(id).await;
+		}
+	}
+}
+
+pub async fn schedule_load(kv: kv::Store, sched_mutex: SchedMutex)
+	-> Result<(), async_nats::Error> {
+
+	let mut ids = kv.keys().await?;
+	let mut sched = sched_mutex.lock().await;
+
+	while let Some(id) = ids.try_next().await? {
+		let val = kv.get(id.clone()).await?.unwrap();
+		let val: KvSched = rmp_serde::from_slice(&val).unwrap();
+		let idx = sched.when.partition_point(|&x| x <= val.when);
+
+		sched.when.insert(idx, val.when);
+		sched.subject.insert(idx, val.subject.to_string());
+		sched.payload.insert(idx, val.payload.clone());
+		sched.id.insert(idx, Some(id.clone()));
+		sched.id_loc.insert(id, idx);
+	}
+
+	Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,141 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+mod gozo;
+use argh::FromArgs;
+use async_nats::jetstream::kv;
+use futures::stream::StreamExt;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+#[derive(FromArgs)]
+/// Message scheduler for NATS
+struct Args {
+	/// set address to NATS server, default: nats://localhost:4222
+	#[argh(option, short='a')]
+	address: Option<String>,
+
+	/// enforce secure (TLS) connection
+	#[argh(switch, short='s')]
+	secure: bool,
+
+	/// set token authentication
+	#[argh(option, short='t')]
+	token: Option<String>,
+
+	/// set username
+	#[argh(option, short='u')]
+	user: Option<String>,
+
+	/// set password
+	#[argh(option, short='p')]
+	password: Option<String>,
+
+	/// set client TLS certificate
+	#[argh(option, short='c')]
+	cert: Option<String>,
+
+	/// set client TLS key
+	#[argh(option, short='k')]
+	key: Option<String>,
+
+	/// set NKey authentication
+	#[argh(option, short='n')]
+	nkey: Option<String>,
+
+	/// set path to credentials file
+	#[argh(option, short='j')]
+	jwt: Option<String>,
+}
+
+impl Args {
+	fn get(&self, field: &str, default: Option<String>) -> Option<String> {
+		let mut value = match field {
+			"address"   => self.address.clone(),
+			"token"     => self.token.clone(),
+			"user"      => self.user.clone(),
+			"password"  => self.password.clone(),
+			"cert"      => self.cert.clone(),
+			"key"       => self.key.clone(),
+			"nkey"      => self.nkey.clone(),
+			"jwt"       => self.jwt.clone(),
+			_           => None,
+		};
+
+		if value.is_none() {
+			let envvar = format!("NATS_{}", field.to_uppercase());
+			value = std::env::var(envvar).ok();
+		}
+
+		if value.is_none() && default.is_some() {
+			value = default;
+		}
+
+		value
+	}
+
+	fn get_bool(&self, field: &str) -> bool {
+		let mut value = match field {
+			"secure"    => self.secure,
+			_           => false,
+		};
+
+		if !value {
+			let envvar = format!("NATS_{}", field.to_uppercase());
+			value = !std::env::var(envvar).unwrap_or("".to_string()).is_empty();
+		}
+
+		value
+	}
+}
+
+#[tokio::main]
+async fn main() -> Result<(), async_nats::Error> {
+	let args: Args = argh::from_env();
+	dotenv::from_filename("./gozo.env").ok();
+	dotenv::from_filename("/etc/gozo.env").ok();
+
+	let address = args.get("address", Some("nats://localhost:4222".to_string())).unwrap();
+
+	let mut options = async_nats::ConnectOptions::new()
+		.require_tls(args.get_bool("secure"));
+
+	if let Some(token) = args.get("token", None) {
+		options = options.token(token);
+	}
+
+	if let (Some(user), Some(password)) = (args.get("user", None), args.get("password", None)) {
+		options = options.user_and_password(user, password);
+	}
+
+	if let (Some(cert), Some(key)) = (args.get("cert", None), args.get("key", None)) {
+    		options = options.add_client_certificate(cert.into(), key.into());
+    	}
+
+	if let Some(nkey) = args.get("nkey", None) {
+		options = options.nkey(nkey);
+	}
+
+	if let Some(jwt) = args.get("jwt", None) {
+		options = options.credentials_file(jwt).await?;
+	}
+
+	let nats = async_nats::connect_with_options(address, options).await?;
+	let jetstream = async_nats::jetstream::new(nats.clone());
+	let kv = jetstream.create_key_value(kv::Config {
+		bucket: "gozo".to_string(),
+		..Default::default()
+	}).await?;
+
+	let sched: gozo::SchedMutex = Arc::new(Mutex::new(gozo::Sched::new()));
+	gozo::schedule_load(kv.clone(), sched.clone()).await?;
+	tokio::spawn(gozo::replyloop(nats.clone(), kv.clone(), sched.clone()));
+
+	let mut sub = nats.subscribe("gozo").await?;
+	while let Some(msg) = sub.next().await {
+		gozo::schedule(kv.clone(), sched.clone(), msg).await;
+	}
+
+	Ok(())
+}
